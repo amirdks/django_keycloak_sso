e9622aac6bae37bc6be954caebfd4b7d72f43d27
amirdks <amirhossein6168@gmail.com>
2025-07-12 15:34:11 +0330

    add require_any_role and fix check_user_permission_access

--- django_keycloak_sso/sso/utils.py
+++ django_keycloak_sso/sso/utils.py
@@ -25,9 +25,18 @@ def check_user_permission_access(
         group_titles: list[str],
         group_roles: list[str],
         match_group_roles: bool = False,
+        permissive: bool = False,
 ) -> bool:
     if not isinstance(user, CustomUser):
         return False
+
+    denied_required_role = False
+    denied_group_title_role = False
+    denied_group_role = False
+
+    require_required_role = False
+    require_group_title_role = False
+    require_group_role = False
 
     # Normalize inputs
     role_titles = [r.lower() for r in role_titles]
@@ -44,20 +53,22 @@ def check_user_permission_access(
             group, role = parts
             role = role[:-1] if role.endswith('s') else role
             parsed_user_groups.append((group.lower(), role.lower()))
-
     # Rule 1: user must have all role_titles in either roles or client_roles
     for required_role in role_titles:
+        require_required_role = True
         if required_role not in user_roles and required_role not in user_client_roles:
-            return False
+            denied_required_role = True
 
     # Rule 2: user must be a member of ALL group_titles (match just the group name)
     user_group_names = [group for group, _ in parsed_user_groups]
     for required_group in group_titles:
+        require_group_title_role = True
         if required_group not in user_group_names:
-            return False
+            denied_group_title_role = True
 
     # Rule 3: user must be in at least one group where their role matches one of group_roles
     if group_roles:
+        require_group_role = True
         matched = False
         for group, role in parsed_user_groups:
             if match_group_roles:
@@ -69,7 +80,15 @@ def check_user_permission_access(
                     matched = True
                     break
         if not matched:
-            return False
+            denied_group_role = True
 
-    # If no matching group-role combo was found
-    return True
+    denied_required_role = denied_required_role if require_required_role else True
+    denied_group_title_role = denied_group_title_role if require_group_title_role else True
+    denied_group_role = denied_group_role if require_group_role else True
+
+    if permissive:
+        access_granted = not (denied_required_role and denied_group_title_role and denied_group_role)
+        return access_granted
+    else:
+        access_granted = not (denied_required_role or denied_group_title_role or denied_group_role)
+        return access_granted
